5ec1e81909325df628e4e94df5f8e984b1e7a0b2
ringosan1974 <wacyakun@gmail.com>
2023-05-03 16:10:58 +0900

    画像生成

--- src/pages/generate.tsx
+++ src/pages/generate.tsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 export default function Gen() {
     const router = useRouter();
     const params = router.query;
-    /*
-    * 画像生成処理をはさむ
-    */
-    console.log(params);
+
+    const [imageURL, setImageURL] = useState<string>();
+    useEffect(() => {
+        const Canvas = document.getElementById("canvas") as HTMLCanvasElement;
+        Canvas.width = 640;
+        Canvas.height = 480;
+
+        const ctx = Canvas.getContext("2d");
+        if (ctx) {
+            const image = new Image();
+            image.onload = () => {
+                ctx.drawImage(image, 0, 0);
+                ctx.fillStyle = "gray";
+                ctx.textAlign = "left";
+                ctx.textBaseline = "top";
+                ctx.fillText("hello world", 120, 200);
+            };
+            image.src = "/resource/test.jpg";
+            setImageURL(Canvas.toDataURL("image/jpeg"));
+        }
+    }, []);
+
     return(
-        <h1>generate page.</h1>
-    )
+        <canvas id="canvas"></canvas>
+    );
 }
